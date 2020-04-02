c828e1086589cbcb470e85b44e2b81ac14dfb9e1
balwantsinghmnit <2016ucp1411@mnit.ac.in>
2020-04-02 10:06:21 +0530

    Updates

---
+++ precache-manifest.93e5ef9fe395371af0e071352c1505db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba09a3434e1240d3d10407ff8340a414",
+    "url": "/react-login-register/index.html"
+  },
+  {
+    "revision": "9ad146d9d555d9d4fd11",
+    "url": "/react-login-register/static/css/main.436d8340.chunk.css"
+  },
+  {
+    "revision": "241d721c16ba39b9971a",
+    "url": "/react-login-register/static/js/2.6f19d665.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-login-register/static/js/2.6f19d665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad146d9d555d9d4fd11",
+    "url": "/react-login-register/static/js/main.c4d2638a.chunk.js"
+  },
+  {
+    "revision": "4b6a7437ec2d9630ead4",
+    "url": "/react-login-register/static/js/runtime-main.adc5a7bf.js"
+  },
+  {
+    "revision": "906381fb91c0102905857aa132d58eb1",
+    "url": "/react-login-register/static/media/plant.906381fb.jpg"
+  }
+]);
